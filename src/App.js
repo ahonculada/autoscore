b77677dd1d0895b6046b7e115d1362d44e274504
aaron <aaron@pop-os.localdomain>
2021-11-12 20:06:46 -0500

    pwa skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { createWorker } from 'tesseract.js';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const worker = createWorker({
-    logger: m => console.log(m),
-  });
-  const doOCR = async () => {
-    await worker.load();
-    await worker.loadLanguage('eng');
-    await worker.initialize('eng');
-    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-    setOcr(text);
-  };
-  const [ocr, setOcr] = useState('Recognizing...');
-  useEffect(() => {
-    doOCR();
-  });
   return (
     <div className="App">
-      <p>{ocr}</p>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
