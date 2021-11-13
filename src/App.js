8cfa322c1427d55c8cf90be4ff2a29416f7fa1dc
tule1102 <tule1102@knights.ucf.edu>
2021-11-12 23:35:51 -0500

    added camera

--- src/App.js
+++ src/App.js
@@ -1,69 +1,76 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef,useState } from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import { WebcamCapture } from './components/Webcam/Webcam';
 
 function App() {
+  // let image = JSON.parse(window.localStorage.getItem('image'));
 
-  const getVideo = () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: { width: 300 } })
-      .then(stream => {
-        let video = videoRef.current;
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch(err => {
-        console.error("error:", err);
-      });
-  };
+  // useEffect(() => {
+  //   setImage(JSON.parse(window.localStorage.getItem('image')));
+  // }, []);
 
-  const paintToCanvas = () => {
-    let video = videoRef.current;
-    let photo = photoRef.current;
-    let ctx = photo.getContext("2d");
+  // const getVideo = () => {
+  //   navigator.mediaDevices
+  //     .getUserMedia({ video: { width: 300 } })
+  //     .then(stream => {
+  //       let video = videoRef.current;
+  //       video.srcObject = stream;
+  //       video.play();
+  //     })
+  //     .catch(err => {
+  //       console.error("error:", err);
+  //     });
+  // };
 
-    const width = 320;
-    const height = 240;
-    photo.width = width;
-    photo.height = height;
+  // const paintToCanvas = () => {
+  //   let video = videoRef.current;
+  //   let photo = photoRef.current;
+  //   let ctx = photo.getContext("2d");
 
-    return setInterval( () => {
-      ctx.drawImage(video, 0, 0, width, height);
-    }, 200);
-  };
+  //   const width = 320;
+  //   const height = 240;
+  //   photo.width = width;
+  //   photo.height = height;
 
-  const takePhoto = () => {
-    let photo = photoRef.current;
-    let strip = stripRef.current;
+  //   return setInterval( () => {
+  //     ctx.drawImage(video, 0, 0, width, height);
+  //   }, 200);
+  // };
 
-    console.warn(strip);
+  // const takePhoto = () => {
+  //   let photo = photoRef.current;
+  //   let strip = stripRef.current;
 
-    const data = photo.toDataURL("image/jpeg", 1.0);
+  //   console.warn(strip);
 
-    console.warn(data);
-    const link = document.createElement("a");
-    link.href = data;
-    link.setAttribute("download", "myWebcam");
-    link.innerHTML = `<img src='${data}' alt='thumbnail'/>`;
-    strip.insertBefore(link, strip.firstChild);
-  };
+  //   const data = photo.toDataURL("image/jpeg", 1.0);
 
-  const videoRef = useRef(null);
-  const photoRef = useRef(null);
-  const stripRef = useRef(null);
+  //   console.warn(data);
+  //   const link = document.createElement("a");
+  //   link.href = data;
+  //   link.setAttribute("download", "myWebcam");
+  //   link.innerHTML = `<img src='${data}' alt='thumbnail'/>`;
+  //   strip.insertBefore(link, strip.firstChild);
+  // };
 
-  useEffect( () => {
-    getVideo();
-  }, [videoRef]
-  );
+  // const videoRef = useRef(null);
+  // const photoRef = useRef(null);
+  // const stripRef = useRef(null);
+
+  // useEffect( () => {
+  //   getVideo();
+  // }, [videoRef]
+  // );
 
   return (
     <div className="App">
-      <button onClick = { () => takePhoto() }>Take a photo</button>
+      {/* <button onClick = { () => takePhoto() }>Take a photo</button>
       <video onCanPlay = { () => paintToCanvas() } ref={videoRef}/>
-      <canvas ref={photoRef} />
+      <canvas ref={photoRef} /> */}
       <div>
-        <div ref={stripRef} />
+        {/* <div ref={stripRef} /> */}
+        <WebcamCapture/>
       </div>
     </div>
   );
