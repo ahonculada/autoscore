528659fcde9b0ade49ccfe88af5cfd2f2d0696e5
aaron <aaron@pop-os.localdomain>
2021-11-13 18:03:03 -0500

    workaround for mobile camera permission

--- src/App.js
+++ src/App.js
@@ -5,8 +5,27 @@ import './App.css';
 import { WebcamCapture } from './components/Webcam/Webcam';
 
 function App () {
+  const [source, setSource] = useState("");
+
+
+  const handleCapture = (target) => {
+    if (target.files) {
+      if (target.files.length !== 0) {
+        const file = target.files[0];
+        const newUrl = URL.createObjectURL(file);
+        setSource(newUrl);
+      }
+    }
+  };
+
   return (
     <div className="App">
+      <input
+            accept="image/*"
+            type="file"
+            capture="environment"
+            onChange={(e) => handleCapture(e.target)}
+          />
       <WebcamCapture/> 
     </div>
   );
